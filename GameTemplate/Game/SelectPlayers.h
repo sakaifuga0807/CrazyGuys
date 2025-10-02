22527ed4233a229458c1de725398ecc29b5d0b9b
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-10-02 15:26:55 +0900

    SelectPlayerの変更

--- GameTemplate/Game/SelectPlayers.h
+++ GameTemplate/Game/SelectPlayers.h
@@ -1,5 +1,24 @@
 #pragma once
-class SelectPlayers
+
+class Title;
+
+class SelectPlayers:public IGameObject
 {
+public:
+
+	enum  EnSelectPlayer
+	{
+
+	};
+
+	bool Start();
+	SelectPlayers();
+	~SelectPlayers();
+	void Update();
+	void Render(RenderContext& rc);
+
+private:
+	//メンバ変数。
+	//SpriteRender m_spriteRender;//スプライトレンダー。
 };
 
