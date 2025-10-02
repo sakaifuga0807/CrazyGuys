4a26914262b10dea610287001b8acd00285f15c8
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-10-02 15:14:44 +0900

    BackGroundの変更

--- GameTemplate/Game/BackGround.cpp
+++ GameTemplate/Game/BackGround.cpp
@@ -1,16 +1,18 @@
 #include "stdafx.h"
 #include "BackGround.h"
+#include <cstdio>.
 
 bool BackGround::Start()
 {
-	//当たり判定を有効にする。
-	//PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
-
 	//ステージの読み込み。
 	m_modelRender.Init("Assets/stageData/StageMini.tkm");
 
+
+	//当たり判定を有効にする。
+	//PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
+
 	//静的物理オブジェクトを作成。
-	m_physicsStaticObject.CreateFromModel(m_modelRender.GetModel(), m_modelRender.GetModel().GetWorldMatrix());
+	m_physicsStaticObj.CreateFromModel(m_modelRender.GetModel(), m_modelRender.GetModel().GetWorldMatrix());
 
 	return true;
 }
