0552a6a4f74623946bd94832ce821b7f5a4268a6
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-10-23 10:39:39 +0900

    Seesawクラスの追加

---
+++ GameTemplate/Game/Seesaw.cpp
@@ -0,0 +1,89 @@
+#include "stdafx.h"
+#include "Seesaw.h"
+#include "Player.h"
+#include "JsonUtility.h"
+
+
+bool Seesaw::Start()
+{
+	m_modelRender.Init("Assets/objectData/Seesaw.tkm");
+
+	json configData;
+	//Jsonファイルを読み込む。
+	if (!JsonUtility::LoadJson("Assets/config/Seesaw.json", configData))
+	{
+		return false;
+	}
+
+	//Seesawの値を変数に格納。
+	auto seesawData = configData["Seesaw"];
+
+	//回転速度。
+	m_angleSpeed = seesawData["AngleSpeed"];
+	//制限角度。
+	m_limitAngle = seesawData["LimitAngle"];
+
+	m_modelRender.SetPosition(m_position);
+
+	m_physicsDynamicObj.CreateFromModel(m_modelRender.GetModel(), m_modelRender.GetModel().GetWorldMatrix());
+
+	//自動で削除されないようにする。
+	m_collisionObject->SetIsEnableAutoDelete(false);
+
+	m_player = FindGO<Player>("player");
+
+	return true;
+}
+
+Seesaw::Seesaw()
+{
+	
+}
+
+Seesaw::~Seesaw()
+{
+
+}
+
+void Seesaw::Update()
+{
+	CheckPlayerOn();
+}
+
+//プレイヤーが乗っているかを確認する関数。
+void Seesaw::CheckPlayerOn()
+{
+	if (m_movingSeesawState == enUp)
+	{
+		m_currentAngle += m_angleSpeed * g_gameTime->GetFrameDeltaTime();
+	}
+	else if(m_movingSeesawState==enDown)
+	{
+		m_currentAngle -= m_angleSpeed * g_gameTime->GetFrameDeltaTime();
+	}
+	if (m_currentAngle >= m_limitAngle)
+	{
+		m_currentAngle = m_limitAngle;
+		m_movingSeesawState = enDown;
+	}
+	else if (m_currentAngle <= -m_limitAngle)
+	{
+		m_currentAngle = -m_limitAngle;
+		m_movingSeesawState = enUp;
+	}
+
+	Quaternion rot;
+	rot.SetRotationDeg(Vector3::AxisX,m_currentAngle);
+
+	//座標と回転を反映させる。
+	m_physicsDynamicObj.SetPosition(m_position);
+	m_physicsDynamicObj.SetRotation(rot);
+	m_modelRender.SetPosition(m_position);
+	m_modelRender.SetRotation(rot);
+	m_modelRender.Update();
+}
+
+void Seesaw::Render(RenderContext& rc)
+{
+	m_modelRender.Draw(rc);
+}
