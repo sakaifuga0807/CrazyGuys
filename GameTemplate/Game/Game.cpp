1bb2a8adc482ae4c52b55a6ccb9f11fe24a3a715
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-10-23 11:02:13 +0900

    Gameの変更

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -5,6 +5,8 @@
 #include "GameCamera.h"
 #include "Goal.h"
 #include "Hammer.h"
+#include"Seesaw.h"
+#include "nature/SkyCube.h"
 
 bool Game::Start()
 {
@@ -18,11 +20,17 @@ bool Game::Start()
 	m_goal = NewGO<Goal>(0, "goal");
 	//ハンマーの生成。
 	m_hammer = NewGO<Hammer>(0, "hammer");
+	m_hammer->SetPosition(Vector3(1253.0, 1.0, -6430.0));
+	//シーソーの生成。
+	m_seesaw = NewGO<Seesaw>(0, "seesaw");
+	m_seesaw->SetPosition(Vector3(496.966187f, 0.0f, -17312.912109f));
 
 	//スカイキューブを作成
 	m_skyCube = NewGO<SkyCube>(0, "skycube");
 	m_skyCube->SetType(enSkyCubeType_DayToon);
-	m_skyCube->SetScale(10000.0f);
+	m_skyCube->SetScale(3800.0f);
+	m_skyCube->SetLuminance(0.5f);
+	m_skyCube->SetPosition(m_player->GetPosition());
 
 	return true;
 }
@@ -39,6 +47,7 @@ Game::~Game()
 	DeleteGO(m_gameCamera);
 	DeleteGO(m_goal);
 	DeleteGO(m_hammer);
+	DeleteGO(m_seesaw);
 	DeleteGO(m_skyCube);
 }
 
